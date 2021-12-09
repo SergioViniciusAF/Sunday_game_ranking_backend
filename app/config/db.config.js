ead1d2cd5b10086a3b9f467c5d00d22101915596
SergioViniciusAF <79481502+SergioViniciusAF@users.noreply.github.com>
2021-12-09 18:34:30 -0300

    Criação de pasta de configurações

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+/* Configurações do banco de dados */
+
+module.exports ={
+    dbURL:'postgres://bmitbkfk:7nH8KbaN-PxMXwr99gWhm_W7Frn29EE9@castor.db.elephantsql.com/bmitbkfk',
+     dialect: "postgres",
+     pool : {
+         max: 5,
+         min:0,
+         acquire: 30000,
+         idle: 10000
+     }
+     
+ };
